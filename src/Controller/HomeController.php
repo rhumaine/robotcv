bcdc883528d4228a965d003f1481cff1d37f0dae
rhurhu <romain.demay56@gmail.com>
2023-01-26 15:54:26 +0100

    session login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,21 +7,29 @@ use App\Entity\Utilisateur;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home_page')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(ManagerRegistry $doctrine,SessionInterface $session): Response
     {
-        $title_page = 'Listes des profils';
 
-        $candidats = $doctrine->getRepository(Candidat::class)->findAll();
+        if($session->get('email') != null){
+            
+            $title_page = 'Listes des profils';
+
+            $candidats = $doctrine->getRepository(Candidat::class)->findAll();
+           
+            return $this->render('liste_profil.html.twig', [
+                'title' => $title_page,
+                'candidats' => $candidats
+            ]);
+        }else{
+            return $this->redirectToRoute('app_login');
+        }
        
-        return $this->render('liste_profil.html.twig', [
-            'title' => $title_page,
-            'candidats' => $candidats
-        ]);
     }
 
 }
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,29 +3,39 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function index(Request $request, SessionInterface $session): Response
     {
 
-        $session = new Session();
-
-        $session->set('email', 'tefggst');
 
         if($_POST){
+            
+            $email = $request->get('email');
+            
+            $session->set('email', $email);
 
-            var_dump('ok');
-            exit;
-        }
+            return $this->redirectToRoute('home_page');
+           
+        }else{
+            return $this->render('login/index.html.twig', [
+                'controller_name' => 'LoginController',
+            ]);
+        }   
+    }
 
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(Request $request, SessionInterface $session): Response
+    {
+        $session = $request->getSession();
+        $session->invalidate();
 
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
-        ]);
+        return $this->redirectToRoute('app_login');
     }
 }
