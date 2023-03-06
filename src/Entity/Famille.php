19abbdffe7e77f9738fd6aa4099737aeff9ae25e
rhurhu <romain.demay56@gmail.com>
2023-03-06 16:41:44 +0100

    add famille

---
+++ src/Entity/Famille.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FamilleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FamilleRepository::class)]
+class Famille
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $famille = null;
+
+    #[ORM\Column]
+    private ?int $parent = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFamille(): ?string
+    {
+        return $this->famille;
+    }
+
+    public function setFamille(string $famille): self
+    {
+        $this->famille = $famille;
+
+        return $this;
+    }
+
+    public function getParent(): ?int
+    {
+        return $this->parent;
+    }
+
+    public function setParent(int $parent): self
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+}
